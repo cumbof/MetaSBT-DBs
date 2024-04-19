463bcde1f645ef2f9cc096fef57ea490233829c6
Fabio Cumbo <fabio.cumbo@gmail.com>
2024-04-19 15:04:01 -0400

    Init Viruses 20240410

--- databases/Viruses/20240410/reproduce/boundaries.sh
+++ databases/Viruses/20240410/reproduce/boundaries.sh
@@ -2,7 +2,7 @@
 
 metasbt boundaries --db-dir ./MetaSBT-Viruses \
                    --superkingdom Viruses \
-                   --nproc 120 \
+                   --nproc 50 \
                    --output ./MetaSBT-Viruses/boundaries.tsv \
                    --log ./MetaSBT-Viruses/boundaries.log \
                    --tmp-dir ./MetaSBT-Viruses/tmp \
---
+++ databases/Viruses/20240410/reproduce/report.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+metasbt report --db-dir ./MetaSBT-Viruses \
+               --output-file ./MetaSBT-Viruses/report.tsv
