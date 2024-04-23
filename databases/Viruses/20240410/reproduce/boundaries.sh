9ad0f76acf3e5a2b2889794680258494f6d451df
Fabio Cumbo <fabio.cumbo@gmail.com>
2024-04-23 12:39:59 -0400

    Removing scripts

--- databases/Viruses/20240410/reproduce/boundaries.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-metasbt boundaries --db-dir ./MetaSBT-Viruses \
-                   --superkingdom Viruses \
-                   --nproc 50 \
-                   --output ./MetaSBT-Viruses/boundaries.tsv \
-                   --log ./MetaSBT-Viruses/boundaries.log \
-                   --tmp-dir ./MetaSBT-Viruses/tmp \
-                   --verbose
--- databases/Viruses/20240410/reproduce/report.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-metasbt report --db-dir ./MetaSBT-Viruses \
-               --output-file ./MetaSBT-Viruses/report.tsv
